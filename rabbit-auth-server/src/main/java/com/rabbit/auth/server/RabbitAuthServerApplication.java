baafb2246a79ff8699a6693fedebe02a309c4b5f
li930615 <39086192+li930615@user.noreply.github.com>
2019-01-19 16:17:36 +0800

    Initial commit

---
+++ rabbit-auth-server/src/main/java/com/rabbit/auth/server/RabbitAuthServerApplication.java
@@ -0,0 +1,14 @@
+package com.rabbit.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitAuthServerApplication.class, args);
+	}
+
+}
+
